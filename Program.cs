6bbc13772e08a732eeae4c10c34a66733230cac9
Mikaele Rodrigues <mikaelerodrigues007@outlook.com>
2025-11-14 21:31:51 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿ string pergunta, resposta, saida;
+
+saida = "tchau";
+ Console.WriteLine("---Groot Chatbot---\n");
+
+while (true)
+{
+    PerguntaUsuario();
+    
+    if (pergunta == saida){
+        resposta = "Eu sou o Groot!";
+        Console.WriteLine($"                       Resposta: {resposta}");
+        break;
+    }
+
+    resposta = "Eu sou o Groot.";
+    Console.WriteLine($"                       Resposta: {resposta}");
+
+}
+
+
+void PerguntaUsuario() {
+    Console.Write("Pergunta: ");
+
+    //variavel só funciona aqui
+    pergunta = Console.ReadLine()!.ToLower().Trim();
+
+} 
+
+
